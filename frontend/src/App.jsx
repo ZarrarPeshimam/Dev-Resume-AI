0c259a5d2e19052bd4277506212236bf1f85f5e1
Zarrar <zarrarpeshimam@gmail.com>
2025-08-21 16:48:03 +0530

    login-signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-const App = () => {
+export default function App() {
   return (
-    <div className='bg-red-400'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
